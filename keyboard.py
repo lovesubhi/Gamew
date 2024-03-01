a2c05af48ca92621dd1ada921fa9f51e75e21d41
lovesubhi <154323448+lovesubhi@users.noreply.github.com>
2024-03-01 10:23:40 +0530

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -4,8 +4,8 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 START_KEYBOARD = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton('Github', GITHUB_URL),
-            InlineKeyboardButton('Developer', DEV_URL)   
+            InlineKeyboardButton('Jᴏɪɴ🔮', GITHUB_URL),
+            InlineKeyboardButton('ᴍᴀsᴛᴇʀ😎, DEV_URL)   
         ]
     ]
 )
